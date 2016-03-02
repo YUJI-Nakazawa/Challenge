7a657bacabe26ed2683320e0a78f47a73b3561dc
YUJI-Nakazawa <yuji.nakazawa1025@gmail.com>
2016-03-02 14:24:05 +0900

    PHPデータ操作の課題8を提出 (簡単な内容ですがよろしくお願いします)

---
+++ challenge_5/5_8challenge.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>課題5-8</title>
+</head>
+<body>
+    <h1>リンクの作成</h1>
+    <a href="https://sites.google.com/a/groove-gear.jp/campfire/">CAMP FIRE</a>
+    
+    <h1>リストの作成</h1>
+    <h4>アンオーダードリスト</h4>
+    <ul>
+        <li>田中</li>
+        <li>小松</li>
+        <li>佐藤</li>
+    </ul>
+    <h4>オーダードリスト</h4>
+    <ol>
+        <li>りんご</li>
+        <li>みかん</li>
+        <li>パイナップル</li>
+    </ol>
+    
+    <h1>表の作成</h1>
+    <table border="1px solid">
+        <tr>
+            <th>セット名</th>
+            <th>内容</th>
+            <th>値段</th>
+        </tr>
+        <tr>
+            <td>Aセット</td>
+            <td>洋食</td>
+            <td>100円</td>
+        </tr>
+        <tr>
+            <td>Bセット</td>
+            <td>中華</td>
+            <td>200円</td>
+        </tr>
+        <tr>
+            <td>Cセット</td>
+            <td>洋食</td>
+            <td>300円</td>
+        </tr>
+    </table>
+    
+</body>
+</html>
